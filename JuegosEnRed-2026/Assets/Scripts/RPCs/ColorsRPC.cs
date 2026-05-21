f26debe8bda2769434b0d1da6e36c7b59e272130
Nahuel-Reimondo-TrickGS <nahuel.reimondo@trickgs.com>
2026-05-21 15:02:31 -0300

    network stats

--- JuegosEnRed-2026/Assets/Scripts/RPCs/ColorsRPC.cs
+++ JuegosEnRed-2026/Assets/Scripts/RPCs/ColorsRPC.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using ExitGames.Client.Photon;
 using Photon.Pun;
+using Photon.Realtime;
 using UnityEngine;
 
 public class ColorsRPC : MonoBehaviourPunCallbacks
@@ -59,6 +61,16 @@ public class ColorsRPC : MonoBehaviourPunCallbacks
     {
         //selectedPhotonView.RPC("ChangeColor", selectedPhotonView.Controller, "Green");
         selectedPhotonView.RPC("ChangeColor", RpcTarget.All, "Green");
+        
+        selectedPhotonView.RPC("ChangeColor", RpcTarget.All, "Blue");
     }
 
+    
+    private void SendMoveUnitsToTargetPositionEvent()
+    {
+        object[] content = new object[] { new Vector3(10.0f, 2.0f, 5.0f), 1, 2, 5, 10 }; 
+        RaiseEventOptions raiseEventOptions = new RaiseEventOptions 
+            { Receivers = ReceiverGroup.All }; 
+        
+    }
 }
